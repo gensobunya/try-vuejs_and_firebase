10a96449596ad76a710dd9dff13bfa64000441f7
gensobunya <gen@gensobunya.net>
2018-08-14 15:22:21 +0900

    P26

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,34 @@
 <template>
-　<div id="app">
-　 <Home v-if="!isLogin"></Home>
-　 <Editor v-if="isLogin"></Editor>
-　</div>
-　</template>
-　
-　<script>
-　import Home from './components/Home.vue';
-　import Editor from './components/Editor.vue';
-　
-　export default {
-　 name: 'app',
-　 data() {
-　 return {
-　 isLogin: false
-　 }
-　 },
-　 components: {
-　 'Home': Home,
-　 'Editor': Editor,
-　 },
-　}
-　</script>
+<div id="app">
+ <Home v-if="!isLogin"></Home>
+ <Editor v-if="isLogin"></Editor>
+</div>
+</template>
+
+<script>
+import Home from './components/Home.vue';
+import Editor from './components/Editor.vue';
+
+export default {
+ name: 'app',
+ data() {
+  return {
+      isLogin: false
+  }
+},
+components: {
+     'Home': Home,
+     'Editor': Editor,
+},
+created: function() {
+ firebase.auth().onAuthStateChanged(user => {
+    console.log(user);
+    if (user) {
+    this.isLogin = true;
+    } else {
+    this.isLogin = false;
+    };
+ });
+},
+}
+</script>
