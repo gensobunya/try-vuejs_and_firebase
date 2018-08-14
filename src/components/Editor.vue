b37ea100d4f9ac220f21ef1aa49de54ed679f642
gensobunya <gen@gensobunya.net>
2018-08-14 16:27:20 +0900

    5.3 メモの削除

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -9,6 +9,7 @@
                 <p class="memoTitle">{{ displayTitle(memo.markdown) }}</p>
             </div>
             <button class="addMemoBtn" @click="addMemo"> メモの追加</button>
+            <button class="deleteMemoBtn" v-if="memos.length > 1" @click="deleteMemo"> 選択中のメモの削除</button>
         </div>
         <textarea class="markdown" v-model="memos[selectedIndex].markdown"></textarea>
         <div class="preview" v-html="preview()"></div>
@@ -24,7 +25,7 @@ export default {
     data() {
         return {
             memos: [{
-            markdown: ''
+                markdown: ''
             }],
             selectedIndex: 0
         }
@@ -46,6 +47,12 @@ export default {
         },
         displayTitle: function(text) {
             return text.split(/\n/)[0];
+        },
+        deleteMemo: function() {
+            this.memos.splice(this.selectedIndex, 1);
+            if (this.selectedIndex > 0) {
+            this.selectedIndex--;
+            }
         },
     }
 }
