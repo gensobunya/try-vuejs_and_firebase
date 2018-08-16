673c0fe05ce52466641e1adc4e18ba6c496d09de
gensobunya <gen@gensobunya.net>
2018-08-16 14:43:04 +0900

    エディタのテスト1

--- spec/Editor/editor.spec.js
+++ spec/Editor/editor.spec.js
@@ -1,17 +1,42 @@
 import { shallowMount } from '@vue/test-utils'
 import Editor from "@/components/Editor.vue"
+import marked from 'marked';
 
 describe("Editor.vue", () => {
+    const user = {
+        displayName:"test user",
+        uid: "testUid0000"
+    }
+    const data = {
+        memos: 
+        [{markdown: ''}],
+        selectedIndex: 0
+    }
+
     it("継承したユーザー名を表示する", () => {
-        const user = {
-            displayName:"test user",
-            uid: "testUid0000"
-        }
+        
         const wrapper = shallowMount(Editor,{
             propsData: {user}
         })
 
-        console.log(wrapper.html())
         expect(wrapper.find("span.displayName").text()).toBe(user.displayName)
+    })
+
+    
+    it("記入したメモのリスト・タイトル・プレビューを表示する", () => {
+        const wrapper = shallowMount(Editor,{
+            propsData:{user},
+            data(){return {data}}
+        })
+        const testmemo = "# test title\n2nd line"
+        wrapper.find(".markdown").setValue(testmemo)
+        data.memos[data.selectedIndex].markdown = testmemo
+
+        console.log(wrapper.html())
+
+        expect(wrapper.find(".memoList").exists()).toBe(true)
+        expect(wrapper.find(".memoTitle").exists()).toBe(true)
+        expect(wrapper.find(".memoTitle").text()).toBe(data.memos[data.selectedIndex].markdown.split(/\n/)[0])
+        expect(wrapper.find(".preview").html()).toBe('<div class="preview">'+marked(testmemo)+'</div>') //markdownをHTMLにレンダリングしたものとレンダリングされたHTMLを比較したいけどDOM抽出した際のdivタグが邪魔
     })
 })
